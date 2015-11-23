a54de11b570aaaafca88cc04b4980c1bb46f6157
Andrey <andrey.paladin@gmail.com>
2015-11-24 00:10:41 +0200

    Palindrome checker

---
+++ untitled2/src/PalindromeChecker.java
@@ -0,0 +1,10 @@
+public class PalindromeChecker{
+    public boolean isAPalindrome(String string) {
+        StringBuffer reversedString = new StringBuffer(string);
+        String reverse = reversedString.reverse().toString();
+        if (reverse.equalsIgnoreCase(string)) {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ untitled2/src/PalindromeCheckerTest.java
@@ -0,0 +1,29 @@
+import org.junit.Test;
+import org.junit.Assert.*;
+
+import static org.junit.Assert.*;
+
+public class PalindromeCheckerTest {
+
+    @Test
+    public void checkPalindromeTestObject(){
+        PalindromeChecker testObject = new PalindromeChecker();
+        assertNotNull(testObject);
+    }
+    @Test
+    public void checkPalindromeTestObject_YesPalindrome(){
+        PalindromeChecker testObject = new PalindromeChecker();
+        assertTrue(testObject.isAPalindrome("MOM"));
+    }
+    @Test
+    public void checkPalindromeTestObject_NOtPalindrome(){
+        PalindromeChecker testobject = new PalindromeChecker();
+        assertFalse(testobject.isAPalindrome("blah"));
+    }
+    @Test
+    public void complexPalindromeTestObject_NOtPalindrome(){
+        PalindromeChecker testobject = new PalindromeChecker();
+        assertFalse(testobject.isAPalindrome("gffhfh"));
+    }
+
+}
