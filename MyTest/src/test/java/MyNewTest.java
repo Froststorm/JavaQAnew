a995516de732bd18ad5b6a920763843f331683e2
Anrii <winged.andrey@gmail.com>
2015-11-26 18:50:01 +0200

    some list change

--- MyTest/src/test/java/MyNewTest.java
+++ MyTest/src/test/java/MyNewTest.java
@@ -18,14 +18,11 @@ public class MyNewTest {
     String baseUrl = "https://www.yahoo.com/";
     String home = "//b";
     List<String> mycoolList = new ArrayList<String>(Arrays.asList(
-            "//li[2]/a/span","//li[3]/a/span","//li[4]/a/span","//li[4]/a/span",
+            "//li[2]/a/span","//li[3]/a/span","//li[4]/a/span","//li[5]/a/span",
             "//li[6]/a/span","//li[7]/a/span","//li[8]/a/span","//li[9]/a/span",
             "//li[10]/a/span","//li[11]/a/span","//li[12]/a/span","//li[13]/a/span",
             "//li[14]/a/span","//li[15]/a/span","//li[16]/a/span","//li[17]/a/span",
             "//li[18]/a/span","//li[19]/a/span"));
-
-
-
 
     @BeforeMethod
     public void beforeTestMethod() {
@@ -39,7 +36,7 @@ public class MyNewTest {
     public void MyTestMethod1() {
         for (int i = 0; i < mycoolList.size(); i++) {
             driver.findElement(By.xpath(mycoolList.get(i))).click();
-            driver.findElement(By.xpath(home)).click();
+            if (i = );
         }
     }
 
