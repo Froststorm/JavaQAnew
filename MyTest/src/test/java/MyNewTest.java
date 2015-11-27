d03a265c8dc6e698e8af0f53ab54829e0f37a2fc
Andrey <winged.andrey@gmail.com>
2015-11-27 07:01:11 +0200

    Подправил название листа(без ворнинга)

--- MyTest/src/test/java/MyNewTest.java
+++ MyTest/src/test/java/MyNewTest.java
@@ -16,8 +16,8 @@ public class MyNewTest {
     WebDriver driver = new FirefoxDriver();
 
     String baseUrl = "https://www.yahoo.com/";
-    String home = "//b[text()='Home']";
-    List<String> mycoolList = new ArrayList<String>(Arrays.asList("//li[1]/a/span",
+    String home = "//*[@id='yucs-top-home']/a/b";
+    List<String> myCoolList = new ArrayList<String>(Arrays.asList("//li[1]/a/span",
             "//li[2]/a/span", "//li[3]/a/span", "//li[4]/a/span", "//li[5]/a/span",
             "//li[6]/a/span", "//li[7]/a/span", "//li[8]/a/span", "//li[9]/a/span",
             "//li[10]/a/span", "//li[11]/a/span", "//li[12]/a/span", "//li[13]/a/span",
@@ -26,25 +26,50 @@ public class MyNewTest {
 
     @BeforeMethod
     public void beforeTestMethod() {
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
         driver.get(baseUrl);
-
     }
 
 
     @Test
     public void MyTestMethod1() {
-        for (int i = 0; i < mycoolList.size(); i++) {
-            if (i == 0 || i == 8) {
-                driver.findElement(By.xpath(mycoolList.get(i))).click();
-                driver.get(baseUrl);
-            }
-            else {
-                driver.findElement(By.xpath(mycoolList.get(i))).click();
-                driver.findElement(By.xpath(home)).click();
 
+        for (int i = 0; i < myCoolList.size(); i++) {
+            long startTime = System.currentTimeMillis() / 1000;
+            System.out.println(" The start time is : " + startTime);
+            if (i == 0 || i == 8) {
+                driver.findElement(By.xpath(myCoolList.get(i))).click();
+                System.out.println(driver.getTitle());
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                driver.get(baseUrl);
+
+
+            } else {
+                driver.findElement(By.xpath(myCoolList.get(i))).click();
+                System.out.println(driver.getTitle());
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                driver.get(baseUrl);
+//                driver.findElement(By.xpath(home)).click();
             }
-            System.out.println(mycoolList.get(i) + " Pass :"+ i);
+
+            long endTime = System.currentTimeMillis() / 1000;
+//            System.out.println(" The end time is : " + endTime);
+            System.out.println("Page load time in seconds " + (endTime-startTime));
+
+            if (endTime-startTime > 7) {
+                System.out.println("Page load time exceeded");
+            } else {
+                System.out.println("Page load time normal");
+            }
+            System.out.println(myCoolList.get(i) + " Pass :" + i);
         }
     }
 
