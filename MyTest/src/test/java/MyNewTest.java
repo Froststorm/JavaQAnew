63cdbe6792d1c6655fe89cc8125b328dc9bd37a6
Andrey <winged.andrey@gmail.com>
2015-11-27 09:08:49 +0200

    Добавил лист второго уровня и закомментированный 2й тест..

--- MyTest/src/test/java/MyNewTest.java
+++ MyTest/src/test/java/MyNewTest.java
@@ -16,13 +16,30 @@ public class MyNewTest {
     WebDriver driver = new FirefoxDriver();
 
     String baseUrl = "https://www.yahoo.com/";
-    String home = "//*[@id='yucs-top-home']/a/b";
-    List<String> firsLevelList = new ArrayList<String>(Arrays.asList("//li[1]/a/span",
-            "//li[2]/a/span", "//li[3]/a/span", "//li[4]/a/span", "//li[5]/a/span",
-            "//li[6]/a/span", "//li[7]/a/span", "//li[8]/a/span", "//li[9]/a/span",
-            "//li[10]/a/span", "//li[11]/a/span", "//li[12]/a/span", "//li[13]/a/span",
-            "//li[14]/a/span", "//li[15]/a/span", "//li[16]/a/span", "//li[17]/a/span",
-            "//li[18]/a/span", "//li[19]/a/span"));
+    String moreYahoo = "//a[contains(text(),'More Yahoo Sites')]";
+    List<String> firsLevelList;
+    List<String> secondLevelList;
+
+    {
+        firsLevelList = new ArrayList<String>(Arrays.asList("//li[1]/a/span",
+                "//li[2]/a/span", "//li[3]/a/span", "//li[4]/a/span", "//li[5]/a/span",
+                "//li[6]/a/span", "//li[7]/a/span", "//li[8]/a/span", "//li[9]/a/span",
+                "//li[10]/a/span", "//li[11]/a/span", "//li[12]/a/span", "//li[13]/a/span",
+                "//li[14]/a/span", "//li[15]/a/span", "//li[16]/a/span", "//li[17]/a/span",
+                "//li[18]/a/span", "//li[19]/a/span"));
+
+        secondLevelList = new ArrayList<String>(Arrays.asList("(//a[contains(text(),'Answers')])[2]",
+                "//a[contains(text(),'Careers')]", "(//a[contains(text(),'Celebrity')])[2]",
+                "//a[contains(text(),'Fantasy Baseball')]", "//a[contains(text(),'Fantasy Sports')]",
+                "(//a[contains(text(),'Flickr')])[2]", "(//a[contains(text(),'Games')])[2]",
+                "(//a[contains(text(),'Groups')])[2]", "//a[contains(text(),'Horoscopes')]",
+                "//a[contains(text(),'Horoscopes')]", "//a[contains(text(),'Local')]",
+                "//a[contains(text(),'Messenger')]", "(//a[contains(text(),'Music')])[2]",
+                "(//a[contains(text(),'My Yahoo')])[2]", "(//a[contains(text(),'Search')])[2]",
+                "//a[contains(text(),'Small Business')]"));
+
+    }
+
 
     @BeforeMethod
     public void beforeTestMethod() {
@@ -30,7 +47,7 @@ public class MyNewTest {
         driver.get(baseUrl);
     }
 
-    @Test
+    @Test(priority = 1)
     public void MyTestMethod1() {
 
         for (int i = 0; i < firsLevelList.size(); i++) {
@@ -61,17 +78,63 @@ public class MyNewTest {
 
             long endTime = System.currentTimeMillis() / 1000;
 //            System.out.println(" The end time is : " + endTime);
-            System.out.println("Page load time in seconds " + (endTime-startTime));
+            System.out.println("Page load time in seconds " + (endTime - startTime));
 
-            if (endTime-startTime > 7) {
+            if (endTime - startTime > 7) {
                 System.out.println("Page load time exceeded");
             } else {
                 System.out.println("Page load time normal");
             }
-            System.out.println(firsLevelList.get(i) + " Pass :" + (i+1));
+            System.out.println(firsLevelList.get(i) + " Pass :" + (i + 1));
         }
     }
 
+    @Test(priority = 2)
+    public void MyTestMethod2() {
+        /**
+         *Пока что закомнтил стрроки второго теста что бы понять могу ли я сделать проверку второго листа
+         * в первом тесте тоже и упростить таки первый тест
+         */
+
+//
+//        for (int i = 0; i < firsLevelList.size(); i++) {
+//            long startTime = System.currentTimeMillis() / 1000;
+//            System.out.println(" The start time is : " + startTime);
+//            if (i == 0 || i == 8) {
+//                driver.findElement(By.xpath(firsLevelList.get(i))).click();
+//                System.out.println(driver.getTitle());
+//                try {
+//                    TimeUnit.SECONDS.sleep(1);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//                driver.get(baseUrl);
+//
+//
+//            } else {
+//                driver.findElement(By.xpath(firsLevelList.get(i))).click();
+//                System.out.println(driver.getTitle());
+//                try {
+//                    TimeUnit.SECONDS.sleep(1);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//                driver.get(baseUrl);
+////                driver.findElement(By.xpath(home)).click();
+//            }
+//
+//            long endTime = System.currentTimeMillis() / 1000;
+////            System.out.println(" The end time is : " + endTime);
+//            System.out.println("Page load time in seconds " + (endTime - startTime));
+//
+//            if (endTime - startTime > 7) {
+//                System.out.println("Page load time exceeded");
+//            } else {
+//                System.out.println("Page load time normal");
+//            }
+//            System.out.println(firsLevelList.get(i) + " Pass :" + (i + 1));
+//        }
+    }
 
     @AfterMethod
     public void afterTestMethod() {
